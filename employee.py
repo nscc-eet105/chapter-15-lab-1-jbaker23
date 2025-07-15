067b3fa6ebba842cdb20d040849141488ba205a4
Jacob Baker <jbaker23@students.northweststate.edu>
2025-07-15 15:28:18 -0400

    Assignment Complete

---
+++ employee.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+@dataclass
+class Employee:
+    _Employee__name: str
+    _Employee__hours_worked: float
+    _Employee__hourly_rate: float
+
+    @property
+    def name(self):
+        return self._Employee__name
+
+    @property
+    def hours_worked(self):
+        return self._Employee__hours_worked
+
+    @property
+    def hourly_rate(self):
+        return self._Employee__hourly_rate
+
+    def calc_pay(self):
+        return self.hours_worked * self.hourly_rate
+
+
+@dataclass
+class Salesperson(Employee):
+    _Salesperson__weekly_sales: float
+    _Salesperson__commission_percentage: float
+
+    @property
+    def weekly_sales(self):
+        return self._Salesperson__weekly_sales
+
+    @property
+    def commission_percentage(self):
+        return self._Salesperson__commission_percentage
+
+    def calc_pay(self):
+        base = super().calc_pay()
+        commission = self.weekly_sales * self.commission_percentage
+        return base + commission
--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import employee
 
-'''
-    CREATING EMPLOYEES
-    Do not edit this section of code. Your classes should work with these tests.
-    If these tests do not work, you must modify/fix the classes you developed.
-'''
-
-employee1 = employee.Employee('George Jetson', 35.50, 22.75)
+employee1 = employee.Employee('George Jetson', 35.5, 22.75)
 employee2 = employee.Employee('Jane Jetson', 40, 30.00)
-sales1 = employee.Salesperson('Leroy Jetson', 25, 15.00, 2550.50, .03)
-sales2 = employee.Salesperson('Judy Jetson', 30, 22.50, 4750.00, .05)
+salesperson1 = employee.Salesperson('Leroy Jetson', 25, 15.00, 2550.50, 0.03)
+salesperson2 = employee.Salesperson('Judy Jetson', 30, 22.50, 4750.00, 0.05)
 
+people = [employee1, employee2, salesperson1, salesperson2]
 
-'''
-    POLYMORPHISM
-    In the area below, create a single list to hold all of the people created 
-    above. Use a single loop to to print the data from each object and the
-    results of the calc_pay method for each of them.
-'''
-
+for person in people:
+    print(person)
+    print(f"Total pay for {person.name} is ${person.calc_pay():.2f}")
+    print("-------------")
